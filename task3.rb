c4187afb6ce0fa93c91e3f811ad280a9ff6aa9fd
Aikidi <aikidi@inbox.ru>
2018-06-08 01:31:21 +0400

    Update task3.rb

--- task3.rb
+++ task3.rb
@@ -1,5 +1,5 @@
 forParse = ARGV.join.scan(/\w/).join
-strVowel = forParse.scan(/[EYUIOAJeuioaj]/).join
-strCnsnnt = forParse.scan(/[^EYUIOAJeuioaj\d]/).join
+strVowel = forParse.scan(/[EYUIOAeyuioa]/).join
+strCnsnnt = forParse.scan(/[^EYUIOAeyuioa\d]/).join
 strNumber = forParse.scan(/\d/).join
 puts strVowel+" "+strCnsnnt+" "+strNumber
