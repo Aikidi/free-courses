64e246711a0be445e14ebf92e99ae4fbf4b63752
Aikidi <aikidi@inbox.ru>
2018-06-07 11:12:09 +0400

    Update task3.rb

--- task3.rb
+++ task3.rb
@@ -1,4 +1,4 @@
-forParse = ARGV.join.split.join.scan(/\w/).join
+forParse = ARGV.join.scan(/\w/).join
 strVowel = forParse.scan(/[EYUIOAJeuioaj]/).join
 strCnsnnt = forParse.scan(/[^EYUIOAJeuioaj\d]/).join
 strNumber = forParse.scan(/\d/).join
