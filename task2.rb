3fd2a4915ace460ec437423eb3db46a11d4d4289
Aikidi <aikidi@inbox.ru>
2018-06-08 01:25:15 +0400

    Update task2.rb

--- task2.rb
+++ task2.rb
@@ -1,5 +1,6 @@
 def signfib(sn)
-    fibn = (1..sn.to_i.abs).inject([0, 1]){ | fbn | fbn << fbn.last(2).inject(:+) }
+    if sn.to_i.zero? then return 0 end
+    fibn = (2..sn.to_i.abs).inject([0, 1]){ | fbn | fbn << fbn.last(2).inject(:+) }
     return ((sn.to_i < 0) && (sn.to_i.abs % 2).zero?) ? -1*fibn[-1] : fibn[-1]
 end
 puts signfib(ARGV[0])
