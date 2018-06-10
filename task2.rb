f86eabe208d3f2072de0e92e9e0b0a59c1dcd39f
Aikidi <aikidi@inbox.ru>
2018-06-11 01:22:36 +0400

    Update task2.rb

--- task2.rb
+++ task2.rb
@@ -1,8 +1,10 @@
-def getFibWS(sn)
-    def fib(n, fbstart = [0,1])
-        n.zero? ? fbstart[1] = 0 : fbstart.push(fbstart.reduce(:+)).shift
-        return n>2 ? fib(n-1, fbstart ) :  fbstart[1]
-    end
-    return ( (sn.to_i<0) && (sn.to_i.abs&1).zero? ) ? -1*fib(sn.to_i.abs) : 1*fib(sn.to_i.abs)
+def fiblambda(n)
+  def fib_gen
+      a, b = 1, 1
+      lambda {ret, a, b = a, b, a+b; ret}
+  end
+  fg = fib_gen
+  (n.to_i.abs-1).times {fg.call}
+  return ( (n.to_i<0) && (n.to_i.abs&1).zero? ) ? -1*fg.call : fg.call
 end
-puts getFibWS(ARGV[0])
+puts fiblambda(ARGV[0])
