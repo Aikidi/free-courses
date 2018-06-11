a060275d38f89b0c61bd79116dac183daa5294ec
Aikidi <aikidi@inbox.ru>
2018-06-11 14:16:50 +0400

    Update task2.rb

--- task2.rb
+++ task2.rb
@@ -1,10 +1,2 @@
-def fiblambda(n)
-  def fib_gen
-      a, b = 1, 1
-      lambda {ret, a, b = a, b, a+b; ret}
-  end
-  fg = fib_gen
-  (n.to_i.abs-1).times {fg.call}
-  return n.to_i.abs.zero? ? 0 : ( ( (n.to_i<0) && (n.to_i.abs&1).zero? ) ? -1*fg.call : fg.call )
-end
-puts fiblambda(ARGV[0])
+require 'matrix'
+puts (Matrix[[0, 1], [1,1]]**(ARGV[0].to_i-1))[1,1].to_i
