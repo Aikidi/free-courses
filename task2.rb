482318d7c287101ab0a7b0ebb3c860f9ad050010
Aikidi <aikidi@inbox.ru>
2018-06-15 23:11:41 +0400

    Update task2.rb

--- task2.rb
+++ task2.rb
@@ -1,5 +1,24 @@
-require 'matrix'
-def getFibMatrix(n)
-  return (Matrix[[0, 1], [1,1]]**(n.to_i-1))[1,1].to_i.to_s
+def getStrSum (fstStr, secStr, resStr = "0")
+    fS, sS, rS = fstStr.reverse, secStr.reverse, resStr
+    i=0
+    begin
+        lR = (fS[i].to_i + sS[i].to_i + rS[i].to_i).to_s.reverse!()
+        rS[i] = lR[0]
+        (lR[1].nil?) ?  rS += "0" : rS += lR[1]
+        i += 1
+    end while (i<=(sS.length-1))
+    return (lR[1].nil?) ? rS.chop!.reverse! : rS.reverse!
 end
-puts getFibMatrix(ARGV[0])
+
+def getFibAccum(n)
+    if n.to_i.abs<2 then return n.to_i.abs end
+    (n.to_i<0) && ((n.to_i.abs&1).zero?) ? sign = '-' : sign = '' ;
+    a = '0' ; b = '1' ; t =''
+    (1..n.to_i.abs-1).each do
+        t, a = a, b
+        b = getStrSum(t, b)
+    end
+    return sign+b
+end
+
+puts getFibAccum(ARGV[0])
