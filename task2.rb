3a235556ba2b74a78d497c7a75f4d2e072a74c5f
Aikidi <aikidi@inbox.ru>
2018-06-16 20:58:56 +0400

    Update task2.rb

--- task2.rb
+++ task2.rb
@@ -1,25 +1,12 @@
-def getStrSum (fstStr, secStr, resStr = "0")
-    fS, sS, rS = fstStr.reverse, secStr.reverse, resStr
-    while (sS.length>fS.length) do fS = fS+"0" end
-    i=0
-    begin
-        lR = (fS[i].to_i + sS[i].to_i + rS[i].to_i) 
-        rS[i] = (lR % 10).to_s
-        rS+=(lR/10).to_i.to_s
-        i += 1
-    end while (i<=(sS.length-1))
-    return ((lR/10).to_i.zero?) ? rS.chop!.reverse! : rS.reverse!
-end
-
-def getFibAccum(n)
-    if n.to_i.abs<2 then return n.to_i.abs end
-    (n.to_i<0) && ((n.to_i.abs&1).zero?) ? sign = '-' : sign = '' ;
-    a = '0' ; b = '1' ; t =''
-    (1..n.to_i.abs-1).each do
-        t, a = a, b
-        b = getStrSum(t, b)
+def getFibGen(n)
+  fib = Enumerator.new do |y|
+    f0, f1 = 0, 1
+    loop do
+      y << f0
+      f0, f1 = f1, f0 + f1
     end
-    return sign+b
+  end
+  return ( (n.to_i<0) && (n.to_i.abs&1).zero? ) ? (-1*fib.lazy.drop(n.to_i.abs).next) : (fib.lazy.drop(n.to_i.abs).next)
 end
 
-puts getFibAccum(ARGV[0])
+puts BigDecimal(getFibGen(ARGV[0])).to_s("F").chop.chop
